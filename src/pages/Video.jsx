d7f47aa3428e994ba39d5c22fe872152bb358103
EdineiPinho <edneyrock@gmail.com>
2023-06-17 23:36:42 -0300

    :sparkles: feat: video play

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './Video.css'
 import video from '../assets/video1.mp4'
 
 function Video() {
+  const [play, setPlay] = useState(false)
+  const videoRef = useRef(null)
+
+  function handleStart() {
+    if (play) {
+      videoRef.current.play()
+      setPlay(!play)
+    } else {
+      videoRef.current.pause()
+      setPlay(!play)
+    }
+  }
+
   return (
     <div className='video'>
       <video
         className='video__player'
+        ref={videoRef}
+        onClick={handleStart}
         loop
         src={video}
       >
